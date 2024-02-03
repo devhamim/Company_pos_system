1651803339daf9a89345d06f1004e5774dd38b7b
devhamim <hamim735@gmail.com>
2024-02-03 19:14:47 +0600

    date filter

--- resources/views/backend/orders/orders_list.blade.php
+++ resources/views/backend/orders/orders_list.blade.php
@@ -389,7 +389,43 @@
     </div>
 
 @endsection
+@section('footer_script')
+<script type="text/javascript">
+    $(document).ready(function () {
+        var start_date = '{{ $defaultStartDate }}';
+        var end_date = '{{ $defaultEndDate }}';
 
+        if (start_date && end_date) {
+            start_date = moment(start_date, 'YYYY-MM-DD');
+            end_date = moment(end_date, 'YYYY-MM-DD');
+        } else {
+            start_date = moment().subtract(6, 'days');
+            end_date = moment();
+        }
+
+        function cb(start, end) {
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+            $('#start_date').val(start.format('YYYY-MM-DD'));
+            $('#end_date').val(end.format('YYYY-MM-DD'));
+        }
+
+        $('#reportrange').daterangepicker({
+            startDate: start_date,
+            endDate: end_date,
+            ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+
+        cb(start_date, end_date);
+    });
+</script>
+@endsection
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         var checkboxes = document.querySelectorAll('.sub_chk');
